99545544d30f401367023e8a64b4b714e9158373
ZacharyGo <ZSGoEmpire@gmail.com>
2024-10-06 15:30:29 +0800

    Rename Project TestConsole to DelegatesConsole

---
+++ DelegatesConsole/DelegatesConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DelegatesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string select = "EventsChained";
+            switch (select)
+            {
+                case "DelegatesBasic":
+                    DelegatesBasic delegatesBasic = new DelegatesBasic();
+                    delegatesBasic.Execute();
+                    break;
+                case "DelegatesAnonymous":
+                    DelegatesAnonymous delegatesAnonymous = new DelegatesAnonymous();
+                    delegatesAnonymous.Execute();
+                    break;
+                case "DelegatesComposable":
+                    DelegatesComposable delegatesComposable = new DelegatesComposable();
+                    delegatesComposable.Execute();
+                    break;
+                case "DelegatesComposable2":
+                    DelegatesComposable2 delegatesComposable2 = new DelegatesComposable2();
+                    delegatesComposable2.Execute();
+                    break;
+                case "EventsBasic":
+                    EventsBasic eventsBasic = new EventsBasic();
+                    eventsBasic.Execute();
+                    break;
+                case "EventsChained":
+                    EventsChained eventsChained = new EventsChained();
+                    eventsChained.Execute();
+                    break;
+            }
+
+        }
+    }
+}
